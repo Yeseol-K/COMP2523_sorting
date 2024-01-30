5862833711e76d3023b2d03bf539b6b8d22e4028
unknown <ye37seol@gmailcom>
2024-01-30 11:33:39 -0800

    Add ISortable

---
+++ LinkedListGroup.ts
@@ -0,0 +1,71 @@
+import { ISortable } from "./src/ISortable";
+
+class Node {
+  next: Node | null = null;
+  data: number;
+  constructor(data: number) {
+    this.data = data;
+  }
+}
+
+export class LinkedListGroup implements ISortable {
+  head: Node | null = null;
+
+  // Create Node out of data and attach to end of list
+  add(data: number): void {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+    tail.next = node;
+  }
+
+  // Should return number of Nodes in List
+  get length(): number {
+    return this.data.length;
+  }
+
+ // Convenience method that returns a Node at a given index
+  at(index: number): Node {
+    if (!this.head) {
+      throw new Error("Error: Index out of bounds");
+    }
+    let counter = 0;
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    throw new Error("Error: Index out of bounds");
+  }
+
+  compare(leftPos: number, rightPos: number): boolean {
+    return this.data[leftPos] > this.data[rightPos];
+  }
+
+  swap(leftPos: number, rightPos: number): void {
+    const temp = this.data[leftPos];
+        this.data[leftPos] = this.data[rightPos];
+        this.data[rightPos] = temp;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
---
+++ src/sortUtil.ts
@@ -0,0 +1,28 @@
+import { CharacterGroup } from "./CharacterGroup";
+import { ISortable } from "./ISortable";
+import { NumberGroup } from "./NumberGroup";
+import { LinkedListGroup } from "../LinkedListGroup";
+
+
+class SortUtil {
+  constructor (collection: LinkedListGroup) {
+    this.collection = collection;
+  }
+
+  sort(): void {
+    const { length } = this.collection;
+    let isSorted = false;
+    let lastUnsorted = length - 1;
+    while (!isSorted) {
+      isSorted = true;
+      for (let i = 0; i < lastUnsorted; i++)
+    }
+
+    if (this.collection[i] > this.compare.collection[i + 1]) {
+      let tempLeft = swap.collection[i];
+      this.collection[i] = this 
+      this.
+
+    }
+  }
+}
